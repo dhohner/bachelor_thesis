1930d33d85f1172fd42a791d31cab92d192fdc7a
dhohner <dah@adorsys.de>
2019-03-05 13:30:58 +0100

    automated contract abi generation for frontend artifacts.require calling convention enforced

---
+++ frontend/src/util/contract.js
@@ -0,0 +1,5 @@
+const config = require('../../../deployed-config')
+const address = config.contracts.company.address
+const abi = config.contracts.company.abi
+
+export { address, abi }
--- migrations/2_deply_contracts.js
+++ migrations/2_deply_contracts.js
@@ -1,6 +1,53 @@
-const dCompany = artifacts.require('./dCompany.sol');
-const BountyFactory = artifacts.require('./BountyFactory/BountyFactory.sol');
+const Company = artifacts.require('Company');
+const BountyFactory = artifacts.require('BountyFactory');
 
-module.exports = function (deployer) {
-	deployer.deploy(BountyFactory);
+const fs = require("fs");
+const path = require("path");
+const configPath = path.join(
+	__dirname,
+	"..",
+	"frontend",
+	"deployed-config.json"
+);
+
+module.exports = async deployer => {
+	await generateContractDeploymentConfig();
+
+	await deployer.deploy(BountyFactory);
+	await deployer.deploy(Company, BountyFactory.address);
+
+	await writeContractInfo(
+		"company",
+		Company.abi,
+		Company.address
+	);
 };
+
+function generateContractDeploymentConfig() {
+	const config = { contracts: {} };
+	return new Promise(resolve => {
+		fs.writeFile(configPath, JSON.stringify(config), "utf8", (err, data) =>
+			resolve()
+		);
+	});
+}
+
+function writeContractInfo(contract, abi, address) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(configPath, "utf8", (err, data) => {
+			if (err) {
+				console.log(err);
+				reject();
+			} else {
+				obj = JSON.parse(data);
+				obj.contracts[contract] = {
+					abi,
+					address
+				};
+				fs.writeFile(configPath, JSON.stringify(obj), "utf8", () =>
+					resolve()
+				);
+			}
+		});
+	});
+}
